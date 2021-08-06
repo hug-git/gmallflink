30a51a911eac4942b9c503f36881f14ad1adaa6d
hug <huguoemail@163.com>
2021-08-06 09:45:07 +0800

    dws ProvinceStats Table(FlinkSQL)

---
+++ realtime/src/main/java/com/hug/app/func/KeywordTableFunction.java
@@ -0,0 +1,24 @@
+package com.hug.app.func;
+
+import com.hug.utils.KeywordUtil;
+import org.apache.flink.table.annotation.DataTypeHint;
+import org.apache.flink.table.annotation.FunctionHint;
+import org.apache.flink.table.functions.TableFunction;
+import org.apache.flink.types.Row;
+
+import java.util.List;
+
+@FunctionHint(output = @DataTypeHint("ROW<word STRING>"))
+public class KeywordTableFunction extends TableFunction<Row> {
+
+    public void eval(String keyword) {
+
+        //切词
+        List<String> words = KeywordUtil.splitKeyword(keyword);
+
+        //遍历写出
+        for (String word : words) {
+            collect(Row.of(word));
+        }
+    }
+}
